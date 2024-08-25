141c676161ceeefc7d3014fd7491e51f736c5834
RobertWesner <robert@wesner.io>
2024-08-25 13:12:08 +0200

    Template erstellt

---
+++ README.md
@@ -0,0 +1,24 @@
+# Spielwiese
+
+> Wenn du möchtest, dass alles weiterhin funktioniert,
+> ändere nichts, dass mit "docker" beginnt.
+> 
+> Alles andere kannst du nach Belieben ändern.
+
+## Spielwiese lokal verwenden
+
+### Starten
+
+```bash
+docker compose up -d
+```
+
+### Stoppen
+
+```bash
+docker compose down
+```
+
+### Öffnen
+
+http://spielwiese.test:80/
---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="de">
+    <head>
+        <title>Spielwiese</title>
+        <style>
+            body {
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Willkommen auf der Spielwiese</h1>
+        <p>
+            PHP-Version: <?= phpversion() ?>
+        </p>
+    </body>
+</html>
